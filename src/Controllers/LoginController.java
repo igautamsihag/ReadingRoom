6ce862c395b5904747c6ef84f00d93f44c643da2
igautamsihag <gautamsihag30@gmail.com>
2024-10-14 14:58:07 +1100

    admin login functionality done

--- src/Controllers/LoginController.java
+++ src/Controllers/LoginController.java
@@ -2,6 +2,8 @@ package Controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -13,10 +15,38 @@ public class LoginController {
     private Button btnsignup;
 	
 	@FXML
+	private Button btnlogin;
+	
+	@FXML
+    private TextField usernamelogin;
+
+    @FXML
+    private PasswordField passwordlogin;
+    
+    @FXML
     public void initialize() {
         btnsignup.setOnAction(event -> goToSignUp());
+        btnlogin.setOnAction(event -> loginManager());
     }
-	
+    
+    @FXML
+    public void loginManager() {
+        String username = usernamelogin.getText();
+        String password = passwordlogin.getText();
+
+        if(adminAuthentication(username, password)) {
+        	try {
+                Parent nextPage = FXMLLoader.load(getClass().getResource("/Views/AdminDashboardView.fxml"));
+                Stage stage = (Stage) btnlogin.getScene().getWindow();
+                stage.setScene(new Scene(nextPage));
+                stage.show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+      
+    }
+    
 	public void goToSignUp() {
         try {
             Parent signupPage = FXMLLoader.load(getClass().getResource("/Views/Signup.fxml"));
@@ -27,5 +57,9 @@ public class LoginController {
             e.printStackTrace();
         }
     }
+	
+	private boolean adminAuthentication(String username, String password) {
+		return username.equals("admin") && password.equals("reading_admin");
+	}
 	
 }
