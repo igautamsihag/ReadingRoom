63f6ff66079cff8f9e947b0ceaa036c38881d26b
igautamsihag <gautamsihag30@gmail.com>
2024-10-14 14:08:36 +1100

    adding signup page design

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,8 +2,8 @@ package application;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
 
@@ -11,10 +11,11 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/Views/Login.fxml"));
+			Parent pane = loader.load();
+			Scene scene = new Scene(pane);
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Log In - The Reading Room");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,7 @@
 module ReadRoomDesktop {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.graphics;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
